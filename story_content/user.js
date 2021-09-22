7fd156b74af80b3d30100ca9c4b0f2225dea17a3
canadianjennifer <canadianjennifer@gmail.com>
2021-09-22 08:58:15 -0400

    update

---
+++ story_content/user.js
@@ -0,0 +1,159 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6Ldtuzz3olf":
+        Script1();
+        break;
+      case "5g5Rala5T0x":
+        Script2();
+        break;
+      case "6OhSl2KGi0T":
+        Script3();
+        break;
+      case "5miCr44h2H5":
+        Script4();
+        break;
+  }
+}
+
+function Script1()
+{
+  var player = GetPlayer();
+
+var uname = player.GetVar("userName");
+
+var pname = player.GetVar("projectName");
+
+var pos = player.GetVar("position");
+
+var q1 = player.GetVar("Q1Full");
+
+var q2 = player.GetVar("Q2Full");
+
+var q3 = player.GetVar("Q3Full");
+
+var q4 = player.GetVar("Q4Full");
+
+var q5 = player.GetVar("Q5Full");
+
+var q6 = player.GetVar("Q6Full");
+
+var q7 = player.GetVar("Q7Full");
+
+var q8 = player.GetVar("Q8Full");
+
+var q9 = player.GetVar("Q9Full");
+
+var q10 = player.GetVar("Q10Full");
+
+var urlstring = ("story_content/external_files/minimal.html?uname=" + uname + "&pname=" + pname+ "&pos=" + pos + "&q1=" + q1  + "&q2=" + q2+ "&q3=" + q3  + "&q4=" + q4  + "&q5=" + q5  + "&q6=" + q6  + "&q7=" + q7  + "&q8=" + q8  + "&q9=" + q9  + "&q10=" + q10);
+
+window.open(urlstring,"minimal.html");
+}
+
+function Script2()
+{
+  var player = GetPlayer();
+
+var uname = player.GetVar("userName");
+
+var pname = player.GetVar("projectName");
+
+var pos = player.GetVar("position");
+
+var q1 = player.GetVar("Q1Full");
+
+var q2 = player.GetVar("Q2Full");
+
+var q3 = player.GetVar("Q3Full");
+
+var q4 = player.GetVar("Q4Full");
+
+var q5 = player.GetVar("Q5Full");
+
+var q6 = player.GetVar("Q6Full");
+
+var q7 = player.GetVar("Q7Full");
+
+var q8 = player.GetVar("Q8Full");
+
+var q9 = player.GetVar("Q9Full");
+
+var q10 = player.GetVar("Q10Full");
+
+var urlstring = ("story_content/external_files/level1.html?uname=" + uname + "&pname=" + pname+ "&pos=" + pos + "&q1=" + q1  + "&q2=" + q2+ "&q3=" + q3  + "&q4=" + q4  + "&q5=" + q5  + "&q6=" + q6  + "&q7=" + q7  + "&q8=" + q8  + "&q9=" + q9  + "&q10=" + q10);
+
+window.open(urlstring,"level1.html");
+}
+
+function Script3()
+{
+  var player = GetPlayer();
+
+var uname = player.GetVar("userName");
+
+var pname = player.GetVar("projectName");
+
+var pos = player.GetVar("position");
+
+var q1 = player.GetVar("Q1Full");
+
+var q2 = player.GetVar("Q2Full");
+
+var q3 = player.GetVar("Q3Full");
+
+var q4 = player.GetVar("Q4Full");
+
+var q5 = player.GetVar("Q5Full");
+
+var q6 = player.GetVar("Q6Full");
+
+var q7 = player.GetVar("Q7Full");
+
+var q8 = player.GetVar("Q8Full");
+
+var q9 = player.GetVar("Q9Full");
+
+var q10 = player.GetVar("Q10Full");
+
+var urlstring = ("story_content/external_files/level2.html?uname=" + uname + "&pname=" + pname+ "&pos=" + pos + "&q1=" + q1  + "&q2=" + q2+ "&q3=" + q3  + "&q4=" + q4  + "&q5=" + q5  + "&q6=" + q6  + "&q7=" + q7  + "&q8=" + q8  + "&q9=" + q9  + "&q10=" + q10);
+
+window.open(urlstring,"level2.html");
+}
+
+function Script4()
+{
+  var player = GetPlayer();
+
+var uname = player.GetVar("userName");
+
+var pname = player.GetVar("projectName");
+
+var pos = player.GetVar("position");
+
+var q1 = player.GetVar("Q1Full");
+
+var q2 = player.GetVar("Q2Full");
+
+var q3 = player.GetVar("Q3Full");
+
+var q4 = player.GetVar("Q4Full");
+
+var q5 = player.GetVar("Q5Full");
+
+var q6 = player.GetVar("Q6Full");
+
+var q7 = player.GetVar("Q7Full");
+
+var q8 = player.GetVar("Q8Full");
+
+var q9 = player.GetVar("Q9Full");
+
+var q10 = player.GetVar("Q10Full");
+
+var urlstring = ("story_content/external_files/level3.html?uname=" + uname + "&pname=" + pname+ "&pos=" + pos + "&q1=" + q1  + "&q2=" + q2+ "&q3=" + q3  + "&q4=" + q4  + "&q5=" + q5  + "&q6=" + q6  + "&q7=" + q7  + "&q8=" + q8  + "&q9=" + q9  + "&q10=" + q10);
+
+window.open(urlstring,"level3.html");
+}
+
